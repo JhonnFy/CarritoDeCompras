7d4ea8ab3a41550dae1f7b697b4215ef2f01b02e
Jhon Fy <moesdeveloper1023@gmail.com>
2026-02-16 18:12:05 -0500

    Clases Completadas

--- CarritoDeCompras/CarritoDeCompras/Entities/CATEGORIA.cs
+++ CarritoDeCompras/CarritoDeCompras/Entities/CATEGORIA.cs
@@ -3,12 +3,8 @@
     public class CATEGORIA
     {
         public int IdCategoria { get; set; }
-
         public string Descripcion { get; set; }
-        
         public bool Activo { get; set; } = true;
-        
         public DateTime FechaRegistro { get; set; } = DateTime.Now;
-
     }
 }
