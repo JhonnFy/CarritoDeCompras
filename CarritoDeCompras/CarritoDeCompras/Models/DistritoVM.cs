b168426393251b8fe4303caf97e0bb2a756f7f21
Jhon Fy <moesdeveloper1023@gmail.com>
2026-02-17 23:07:45 -0500

    DistritoVM OK

--- CarritoDeCompras/CarritoDeCompras/Models/DistritoVM.cs
+++ CarritoDeCompras/CarritoDeCompras/Models/DistritoVM.cs
@@ -2,5 +2,9 @@
 {
     public class DistritoVM
     {
+        public int IdDistrito { get; set; }
+        public string Descripcion { get; set; }
+        public int IdProvincia { get; set; }
+        public int IdDepartamento { get; set; }
     }
 }
