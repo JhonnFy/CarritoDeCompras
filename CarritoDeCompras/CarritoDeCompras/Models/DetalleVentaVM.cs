0644fd4d098e5c5dea97813dcd132e395cb4bf09
Jhon Fy <moesdeveloper1023@gmail.com>
2026-02-17 22:59:00 -0500

    DetalleVenta OK

--- CarritoDeCompras/CarritoDeCompras/Models/DetalleVentaVM.cs
+++ CarritoDeCompras/CarritoDeCompras/Models/DetalleVentaVM.cs
@@ -2,5 +2,11 @@
 {
     public class DetalleVentaVM
     {
+        public int IdDetalleVenta { get; set; }
+        public int IdVenta { get; set; }
+        public int IdProducto { get; set; }
+        public int Cantidad { get; set; }
+        public int Total { get; set; }
+
     }
 }
