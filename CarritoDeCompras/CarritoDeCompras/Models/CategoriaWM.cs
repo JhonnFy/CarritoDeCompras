e67e792fac7c6b9dabc304b4baa68af1ecfb33e9
Jhon Fy <moesdeveloper1023@gmail.com>
2026-02-17 21:32:39 -0500

    Clase CategoriaVM

---
+++ CarritoDeCompras/CarritoDeCompras/Models/CategoriaWM.cs
@@ -0,0 +1,16 @@
+﻿
+using CarritoDeCompras.Entities;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace CarritoDeCompras.Models
+{
+    public class CategoriaWM
+    {
+        public int IdCategoria { get; set; }
+        [Required]
+        public string Descripcion { get; set; }
+        public bool Activo { get; set; }
+        public DateTime FechaRegistro { get; set; } = DateTime.Now;
+    }
+}
